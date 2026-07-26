98cb2c28e1d3c6e25dd563895db0430e14c1c44d
LordGew <1992jairoh@gmail.com>
2026-07-26 18:32:09 -0500

    Add Vercel Analytics to Angular frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
 import { registerLocaleData } from "@angular/common";
 import localeEsCo from "@angular/common/locales/es-CO";
+import { isDevMode } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
+import { inject as injectVercelAnalytics } from "@vercel/analytics";
 
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 
 registerLocaleData(localeEsCo);
+injectVercelAnalytics({ mode: isDevMode() ? "development" : "production" });
 
 bootstrapApplication(AppComponent, appConfig).catch((error) => console.error(error));
